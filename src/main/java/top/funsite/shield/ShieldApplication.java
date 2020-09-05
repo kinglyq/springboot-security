b65d088fcf85d8fc18c758796b210bd0ff3a8a91
kinglyq <755365600@qq.com>
2020-09-05 21:19:49 +0800

    建立一个spring boot应用

---
+++ src/main/java/top/funsite/shield/ShieldApplication.java
@@ -0,0 +1,13 @@
+package top.funsite.shield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldApplication.class, args);
+	}
+
+}
