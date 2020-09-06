862da226e2ff90c326e070b093652df41fc448cf
kinglyq <755365600@qq.com>
2020-09-06 22:35:54 +0800

    添加SecurityConfig

---
+++ src/main/java/top/funsite/shield/SecurityConfig.java
@@ -0,0 +1,50 @@
+package top.funsite.shield;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author Li Yuqing
+ * @date 2020/9/6 20:32:03
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 排除的接口
+        String[] permits = {
+                "/h2/**"
+        };
+        http.authorizeRequests(auth -> auth.
+                antMatchers(permits).permitAll()
+                .anyRequest().authenticated())
+                .headers(header -> {
+                    // 防止打不开h2控制台页面
+                    header.frameOptions().disable();
+                })
+                // 禁用csrf
+                .csrf(AbstractHttpConfigurer::disable);
+    }
+
+    /**
+     * 跨域配置
+     *
+     * @return 基于URL的跨域配置信息
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 注册跨域配置
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
