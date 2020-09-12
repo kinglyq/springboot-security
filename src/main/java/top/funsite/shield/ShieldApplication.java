62ef5f9dbd3e760378f574b57dad757577d67bff
kinglyq <755365600@qq.com>
2020-09-12 22:43:18 +0800

    add author

--- src/main/java/top/funsite/shield/ShieldApplication.java
+++ src/main/java/top/funsite/shield/ShieldApplication.java
@@ -3,6 +3,9 @@ package top.funsite.shield;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Li Yuqing
+ */
 @SpringBootApplication
 public class ShieldApplication {
 
